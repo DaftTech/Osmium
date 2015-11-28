33e176aafcf50edce95119a1d492f0af92aba1f4
Fabian Sachara <Fabian@Sachara.de>
2015-11-28 18:40:05 +0100

    GDT

--- kernel/clevel_entry.c
+++ kernel/clevel_entry.c
@@ -1,9 +1,16 @@
 #include "level0/console.h"
+#include "level0/gdt.h"
 
 #include "multiboot.h"
 
 void clevel_entry(struct multiboot_info* mb_info) {
 	clrscr();
-
+	setclr(C_DEFAULT);
 	kprintf("LEVEL0 ENTRY\n");
+
+	kprintf("GDT INIT ");
+	init_gdt();
+	setclr(C_SUCCESS);
+	kprintf("DONE!");
+	setclr(C_DEFAULT);
 }
--- kernel/include/level0/console.h
+++ kernel/include/level0/console.h
@@ -1,6 +1,28 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#define SCLR_BLACK 0x0
+#define SCLR_BLUE 0x1
+#define SCLR_GREEN 0x2
+#define SCLR_CYAN 0x3
+#define SCLR_RED 0x4
+#define SCLR_MAGENTA 0x5
+#define SCLR_BROWN 0x6
+#define SCLR_LGREY 0x7
+#define SCLR_DGREY 0x8
+#define SCLR_LBLUE 0x9
+#define SCLR_LGREEN 0xA
+#define SCLR_LCYAN 0xB
+#define SCLR_LRED 0xC
+#define SCLR_LMAGENTA 0xD
+#define SCLR_YELLOW 0xE
+#define SCLR_WHITE 0xF
+
+#define COLOR(B, F) ((B << 4) + F)
+
+#define C_DEFAULT COLOR(SCLR_BLACK, SCLR_WHITE)
+#define C_SUCCESS COLOR(SCLR_BLACK, SCLR_LGREEN)
+
 #include "stdarg.h"
 
 void clrscr(void);
