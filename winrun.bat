041b1bdb0d87671157b578cba4c5517d6aa07eb4
Fabian Sachara <fabian@sachara.de>
2018-02-18 12:32:26 +0100

    update for new building environment

--- winrun.bat
+++ winrun.bat
@@ -1,2 +1,2 @@
-start "" qemu/qemu-system-i386.exe -kernel bin/kernel -initrd bin/initrfs.tar -monitor telnet:127.0.0.1:1234,server,nowait -m 1024 --no-reboot --no-shutdown
+start "" qemu-system-i386 -kernel bin/kernel -initrd bin/initrfs.tar -monitor telnet:127.0.0.1:1234,server,nowait -m 1024 --no-reboot --no-shutdown
 puttytel -telnet 127.0.0.1 1234
